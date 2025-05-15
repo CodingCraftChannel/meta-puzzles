1eb3c5ae61de089fa57c43f07853273f0201bf46
Konstantin Burov <burov.konstantin@gmail.com>
2025-05-15 12:50:10 -0400

    Update level_1.py
    Add some comments

--- stack-stabilization/level_1.py
+++ stack-stabilization/level_1.py
@@ -2,16 +2,16 @@ from typing import List
 
 def getMinimumDeflatedDiscCount(N: int, R: List[int]) -> int:
   count = 0
-  prev_disk_size = R[-1]
+  max_allowed = R[-1]           # Start from the bottom disc
 
   for i in range(N - 1, 0, -1):
-      if R[i] <= i:
-          return -1
+    if R[i] <= i:
+      return -1                 # Invalid disc size: radius too small
         
-      if R[i - 1] >= prev_disk_size:
-          count += 1
-          prev_disk_size = prev_disk_size - 1
-      else:
-          prev_disk_size = R[i - 1]
+    if R[i - 1] >= max_allowed:
+      count += 1                # Deflate to make strictly decreasing
+      max_allowed -= 1
+    else:
+      max_allowed = R[i - 1]
 
   return count
